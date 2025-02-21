10b66a37ef016d43799c74e0891f8c8ab16a1e9a
francesco7773434 <francescoorbello5@gmail.com>
2025-02-21 21:09:25 +0100

    21.09

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,8 @@
 export const SELECT_TRACK = "SELECT_TRACK";
 export const GALLERY_TRACK = "GALLERY_TRACK";
+export const GALLERY_TRACK_QUEEN = "GALLERY_TRACK_QUEEN";
+export const GALLERY_TRACK_EMINEM = "GALLERY_TRACK_EMINEM";
+export const GALLERY_TRACK_KATY_PERRY = "GALLERY_TRACK_KATY_PERRY";
 
 export const MusicSectionAction = (artistName) => {
   return async (dispatch) => {
@@ -11,13 +14,13 @@ export const MusicSectionAction = (artistName) => {
         const topTracks = data.slice(0, 4);
         switch (artistName) {
           case "Queen":
-            dispatch({ type: "GALLERY_TRACK_QUEEN", payload: topTracks });
+            dispatch({ type: GALLERY_TRACK_QUEEN, payload: topTracks });
             break;
           case "Eminem":
-            dispatch({ type: "GALLERY_TRACK_EMINEM", payload: topTracks });
+            dispatch({ type: GALLERY_TRACK_EMINEM, payload: topTracks });
             break;
           case "Katy Perry":
-            dispatch({ type: "GALLERY_TRACK_KATY_PERRY", payload: topTracks });
+            dispatch({ type: GALLERY_TRACK_KATY_PERRY, payload: topTracks });
             break;
           default:
             console.error("artista non trovato");
